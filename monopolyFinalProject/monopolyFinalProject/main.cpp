a0ffdbeacc3e9d7dd4d2c6630c62ca2256862f75
Saqib Ali <business.saqib@gmail.com>
2020-05-13 14:58:15 +0500

    Implemented Load Game Screen
    Load Game Screen displays all the previously stored game files in form of a list.
    Load Game Screen handles lack of any saved files as well by directing to the "New Game" screen.
    Load Game Screen has clickability and functionality to tell which option was clicked.

--- monopolyFinalProject/monopolyFinalProject/main.cpp
+++ monopolyFinalProject/monopolyFinalProject/main.cpp
@@ -1,5 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
+#include <cstring>
+
 
 using namespace std;
 
@@ -10,7 +13,11 @@ int main()
     // Creating an SFML window
     sf::RenderWindow window(sf::VideoMode(1280, 720),
         "Monopoly by Saqib, Nabeel, Abdur Rehman, & Salman",
-        sf::Style::Close | sf::Style::Resize);
+        sf::Style::Close | sf::Style::Titlebar);
+
+    // Standard Font
+    sf::Font stdFont;
+    stdFont.loadFromFile ("fonts/Montserrat-Black.ttf");
 
 
     // Implementing the favicon
@@ -148,72 +155,132 @@ int main()
 
     }
 
+    gameStarted = false;
 
-    if (choice == 1) {
-    
+    while (!gameStarted) {
 
-        // Keeps the total number of players
-        // that the user has asked for
-        // the game supports 3 - 5 players.
-        int totalPlayers = -1;
+        if (choice == 1) {
 
-        // Background screen
-        sf::Texture playerNumberScreenTexture;
-        playerNumberScreenTexture.loadFromFile("assets/player-number-selection-screen.png");
-        sf::RectangleShape playerNumberScreen(sf::Vector2f(1280.0f, 720.0f));
-        playerNumberScreen.setTexture(&playerNumberScreenTexture);
+            gameStarted = true;
+
+            // Keeps the total number of players
+            // that the user has asked for
+            // the game supports 3 - 5 players.
+            int totalPlayers = -1;
+
+            // Background screen
+            sf::Texture playerNumberScreenTexture;
+            playerNumberScreenTexture.loadFromFile("assets/player-number-selection-screen.png");
+            sf::RectangleShape playerNumberScreen(sf::Vector2f(1280.0f, 720.0f));
+            playerNumberScreen.setTexture(&playerNumberScreenTexture);
 
 
-        // Card that shows 3 players
-        sf::Texture threePlayerTexture;
-        threePlayerTexture.loadFromFile("assets/3-player-card.png");
-        sf::RectangleShape threePlayerCard(sf::Vector2f(251.0f, 272.0f));
-        threePlayerCard.setTexture(&threePlayerTexture);
-        threePlayerCard.setPosition(sf::Vector2f(250.0f, 200.0f));
+            // Card that shows 3 players
+            sf::Texture threePlayerTexture;
+            threePlayerTexture.loadFromFile("assets/3-player-card.png");
+            sf::RectangleShape threePlayerCard(sf::Vector2f(251.0f, 272.0f));
+            threePlayerCard.setTexture(&threePlayerTexture);
+            threePlayerCard.setPosition(sf::Vector2f(250.0f, 200.0f));
 
-        // Card that shows 4 players
-        sf::Texture fourPlayerTexture;
-        fourPlayerTexture.loadFromFile("assets/4-player-card.png");
-        sf::RectangleShape fourPlayerCard(sf::Vector2f(251.0f, 272.0f));
-        fourPlayerCard.setTexture(&fourPlayerTexture);
-        fourPlayerCard.setPosition(sf::Vector2f(
-            threePlayerCard.getPosition().x + 260, threePlayerCard.getPosition().y));
+            // Card that shows 4 players
+            sf::Texture fourPlayerTexture;
+            fourPlayerTexture.loadFromFile("assets/4-player-card.png");
+            sf::RectangleShape fourPlayerCard(sf::Vector2f(251.0f, 272.0f));
+            fourPlayerCard.setTexture(&fourPlayerTexture);
+            fourPlayerCard.setPosition(sf::Vector2f(
+                threePlayerCard.getPosition().x + 260, threePlayerCard.getPosition().y));
 
-        // Card that shows 5 players
-        sf::Texture fivePlayerTexture;
-        fivePlayerTexture.loadFromFile("assets/5-player-card.png");
-        sf::RectangleShape fivePlayerCard(sf::Vector2f(251.0f, 272.0f));
-        fivePlayerCard.setTexture(&fivePlayerTexture);
-        fivePlayerCard.setPosition(sf::Vector2f(
-            fourPlayerCard.getPosition().x + 260, threePlayerCard.getPosition().y));
+            // Card that shows 5 players
+            sf::Texture fivePlayerTexture;
+            fivePlayerTexture.loadFromFile("assets/5-player-card.png");
+            sf::RectangleShape fivePlayerCard(sf::Vector2f(251.0f, 272.0f));
+            fivePlayerCard.setTexture(&fivePlayerTexture);
+            fivePlayerCard.setPosition(sf::Vector2f(
+                fourPlayerCard.getPosition().x + 260, threePlayerCard.getPosition().y));
 
-        while (totalPlayers == -1) {
-        
+            while (totalPlayers == -1) {
 
-            sf::Event evt;
-            while (window.pollEvent(evt)) {
-            
 
-                if (evt.type == evt.Closed) {
+                sf::Event evt;
+                while (window.pollEvent(evt)) {
 
-                    window.close();
+
+                    if (evt.type == evt.Closed) {
+
+                        window.close();
+
+                    }
+
+                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+
+
+                        sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+                        // Checks if any of the cards have been clicked
+
+                        sf::FloatRect threeCardBound = threePlayerCard.getGlobalBounds();
+                        sf::FloatRect fourCardBound = fourPlayerCard.getGlobalBounds();
+                        sf::FloatRect fiveCardBound = fivePlayerCard.getGlobalBounds();
+
+                        if (threeCardBound.contains(mousePos)) totalPlayers = 3;
+                        if (fourCardBound.contains(mousePos)) totalPlayers = 4;
+                        if (fiveCardBound.contains(mousePos)) totalPlayers = 5;
+
+
+                    }
+
 
                 }
 
-                if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                window.draw(playerNumberScreen);
+                window.draw(threePlayerCard);
+                window.draw(fourPlayerCard);
+                window.draw(fivePlayerCard);
+                window.display();
+
+            }
 
 
-                    sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+        }
+        else {
 
-                    // Checks if any of the cards have been clicked
+            window.clear();
 
-                    sf::FloatRect threeCardBound = threePlayerCard.getGlobalBounds();
-                    sf::FloatRect fourCardBound = fourPlayerCard.getGlobalBounds();
-                    sf::FloatRect fiveCardBound = fivePlayerCard.getGlobalBounds();
+            ifstream fin;
+            fin.open("savedGames/savedGamesList.txt");
 
-                    if (threeCardBound.contains(mousePos)) totalPlayers = 3;
-                    if (fourCardBound.contains(mousePos)) totalPlayers = 4;
-                    if (fiveCardBound.contains(mousePos)) totalPlayers = 5;
+
+            // Getting the total number of games
+            // that is written on top
+
+            int totalGames = -1;
+
+            if (!fin.eof())
+                fin >> totalGames;
+
+            fin.ignore();
+
+
+            // Reading all the saved games
+            // from the savedGamesList.txt file
+
+            char** list = nullptr;
+
+            if (totalGames > 0) {
+                list = new char* [totalGames];
+
+                int i = 0;
+
+                while (!fin.eof()) {
+
+                    char buff[100];
+
+                    fin.getline(buff, 100);
+
+                    list[i] = new char[strlen(buff) + 1];
+                    strcpy_s(list[i], strlen(buff) + 1, buff);
+
+                    i++;
 
 
                 }
@@ -221,18 +288,157 @@ int main()
 
             }
 
-            window.draw(playerNumberScreen);
-            window.draw(threePlayerCard);
-            window.draw(fourPlayerCard);
-            window.draw(fivePlayerCard);
-            window.display();
+            fin.close();
+
+            // Background screen
+            sf::Texture playerNumberScreenTexture;
+            playerNumberScreenTexture.loadFromFile("assets/player-number-selection-screen.png");
+            sf::RectangleShape playerNumberScreen(sf::Vector2f(1280.0f, 720.0f));
+            playerNumberScreen.setTexture(&playerNumberScreenTexture);
+
+
+            // New game button
+            sf::Texture newGameButtonTexture;
+            newGameButtonTexture.loadFromFile("assets/new-game-button.png");
+            sf::RectangleShape newGameButton(sf::Vector2f(316.0f, 94.0f));
+            newGameButton.setTexture(&newGameButtonTexture);
+
+            newGameButton.setPosition(sf::Vector2f(480.0f, 400.0f));
+
+
+
+            if (!list) {
+
+                sf::Text noGamesTxt;
+                noGamesTxt.setString("No games saved previously!");
+                noGamesTxt.setFillColor(sf::Color::Red);  // Setting the text color to cream
+                noGamesTxt.setFont(stdFont);
+
+                noGamesTxt.setPosition(sf::Vector2f(400.0f, 250.0f));
+
+                while (choice != 1) {
+
+                    sf::Event evt;
+                    while (window.pollEvent(evt)) {
+
+                        if (evt.type == evt.Closed) {
+
+                            window.close();
+
+                        }
+
+                        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                        
+
+                            sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+                            // Checking if the new button has been clicked.
+
+                            sf::FloatRect newGameButtonBounds = newGameButton.getGlobalBounds();
+
+
+                            if (newGameButtonBounds.contains(mousePos)) {
+
+                                choice = 1;
+
+                            }
+
+
+
+                        }
+
+                    }
+
+                    window.draw(playerNumberScreen);
+                    window.draw(noGamesTxt);
+                    window.draw(newGameButton);
+                    window.display();
+
+                }
+
+
+            }
+            else {
+
+                sf::Text totalGamesText;
+                totalGamesText.setString("~ SAVED GAMES ~");
+
+                totalGamesText.setCharacterSize(35);
+                totalGamesText.setFont(stdFont);
+                totalGamesText.setFillColor(sf::Color(163, 191, 46));
+                totalGamesText.setPosition(sf::Vector2f(470.0f, 130.0f));
+
+                sf::Text *gamesListText = new sf::Text[totalGames];
+
+                for (int i = 0; i < totalGames; i++) {
+                
+                    gamesListText[i].setCharacterSize(30);
+                    gamesListText[i].setFont(stdFont);
+                    gamesListText[i].setFillColor(sf::Color(94, 60, 54));
+                    gamesListText[i].setString(list[i]);
+                    gamesListText[i].setPosition(sf::Vector2f(500.0f, 220.0f + ((float)i * 50.0f)));
+
+                }
+
+
+                int gameChoiceSelected = -1;
+
+                while (!gameStarted) {
+                
+                    sf::Event evt;
+                    while (window.pollEvent(evt)) {
+                    
+                        if (evt.type == evt.Closed) {
+                        
+                            window.close();
+
+                        }
+
+                        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                            
+                            sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+                            // Checking if any of the game selections were used.
+
+                            for (int i = 0; i < totalGames; i++) {
+
+                                sf::FloatRect gamesListTextBound = gamesListText[i].getGlobalBounds();
+
+                                if (gamesListTextBound.contains(mousePos)) {
+                                    gameChoiceSelected = i;
+                                    gameStarted = true;
+                                }
+
+
+                            }
+
+
+                        }
+
+                    }
+
+                    window.draw(playerNumberScreen);
+
+                    window.draw(totalGamesText);
+
+                    for (int i = 0; i < totalGames; i++) {
+                    
+                        window.draw(gamesListText[i]);
+
+                    }
+
+                    window.display();
+
+                }
+
+            }
+
+
+
 
         }
 
-
     }
-
-
 
     return 0;
 }
